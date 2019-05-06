46951791b32c20ba5ee84423d41396923db691e8
Artem Batalov <artem.batalov.prof@gmail.com>
2019-05-06 21:46:47 +0300

    Initial commit

---
+++ src/main/java/io/cell/imagesplitter/ImagesplitterApplication.java
@@ -0,0 +1,13 @@
+package io.cell.imagesplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagesplitterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ImagesplitterApplication.class, args);
+  }
+
+}
